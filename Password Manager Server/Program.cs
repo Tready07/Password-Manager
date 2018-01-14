78ec655c8e7b0eae94c0ed2109b7c242933aa7ee
Richard Lim <rclim95@outlook.com>
2018-01-14 13:20:41 -0600

    Gracefully end the server when the user presses CTRL+C
    That way, all long-running Tasks are stopped properly when the user
    requests to close the server (by pressing CTRL+C).

--- Password Manager Server/Program.cs
+++ Password Manager Server/Program.cs
@@ -7,18 +7,27 @@ namespace Password_Manager_Server
 {
     class Program
     {
-        static async Task Main()
+        private static Server server;
+
+        public static async Task Main()
         {
 #if DEBUG
             var consoleListener = new ConsoleTraceListener();
             Debug.Listeners.Add(consoleListener);
 #endif
-
             bool isInitialized = databaseInitializer.intializeDatabase();
+
             var con = new SQLiteConnection();
             con = databaseInitializer.makeConnection(con);
-            Server server = new Server();
-            await server.Start();
+
+            server = new Server();
+            server.Start();
+        }
+
+        private static void OnCancelKeyPressed(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.WriteLine("Stopping the server...");
+            server.Stop();
         }
     }
 }
