e83424d12adf2c05958a43d369d368b2df628861
Richard Lim <rclim95@outlook.com>
2018-01-31 22:59:54 -0600

    Show an error if the user enters a bad username or password
    The server will force a disconnect if bad authentication information is
    supplied, so add an event listener so that we can show a message to the
    user when that happens.

--- Password Manager/LoginDialog.cs
+++ Password Manager/LoginDialog.cs
@@ -20,7 +20,7 @@ namespace Password_Manager
         }
 
         private async void loginSubmitButton(object sender, EventArgs e)
-        {
+        { 
             Console.WriteLine("The Submit button was clicked!");
             String username = this.usernameTextBox.Text;
             String password = this.passwordTextBox.Text;
@@ -28,8 +28,24 @@ namespace Password_Manager
             LoginRequest msg = new LoginRequest(user);
             SocketManager sktMngr = SocketManager.Instance;
             sktMngr.connect(this.serverAddressTextBox.Text, (int)serverPort.Value);
+
+            // If we get disconnected from the server, then that means we entered some bad login
+            // information.
+            EventHandler badAuthentication = null;
+            badAuthentication = (s, ea) =>
+            {
+                // Disconnect from the Disconnected event so that we're not emitted if we get
+                // disconnected again.
+                sktMngr.Disconnected -= badAuthentication;
+
+                MessageBox.Show(
+                    "Unable to sign in\n\nThe username or password is incorrect.", 
+                    "Can't Sign In", 
+                    MessageBoxButtons.OK);
+            };
+            sktMngr.Disconnected += badAuthentication;
+
             await sktMngr.SendMessage(msg);
         }
-
     }
 }
