b0de7c3aca9ed344c4a23f25b057571182eb0f4b
Richard Lim <rclim95@outlook.com>
2018-02-22 23:36:45 -0600

    Redesign the UI of the LoginDialog
    - Make it look similar to the dialog style used in Windows Vista and
      higher (i.e., blue main instruction text, white dialog background,
      etc.) for more consistency with other Windows desktop application
    - Combine the "Server Address" and "Server Port" to a single row instead
      of having it take two rows
    - Change the tab order to accomodate for the new layout of the
      LoginDialog
    - Shrink the LoginDialog so that it fits its content nicely

--- Password Manager/LoginDialog.Designer.cs
+++ Password Manager/LoginDialog.Designer.cs
@@ -34,81 +34,76 @@
             this.usernameLabel = new System.Windows.Forms.Label();
             this.passwordLabel = new System.Windows.Forms.Label();
             this.serverAddressTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.serverPort = new System.Windows.Forms.NumericUpDown();
-            this.label2 = new System.Windows.Forms.Label();
+            this.labelServer = new System.Windows.Forms.Label();
+            this.labelMainInstruction = new System.Windows.Forms.Label();
+            this.labelInstruction = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.serverPort)).BeginInit();
             this.SuspendLayout();
             // 
             // submitButton
             // 
-            this.submitButton.Location = new System.Drawing.Point(92, 207);
+            this.submitButton.Location = new System.Drawing.Point(344, 203);
+            this.submitButton.Margin = new System.Windows.Forms.Padding(3, 20, 3, 10);
             this.submitButton.Name = "submitButton";
-            this.submitButton.Size = new System.Drawing.Size(100, 22);
-            this.submitButton.TabIndex = 0;
-            this.submitButton.Text = "Submit";
+            this.submitButton.Size = new System.Drawing.Size(84, 25);
+            this.submitButton.TabIndex = 9;
+            this.submitButton.Text = "&Login";
             this.submitButton.UseVisualStyleBackColor = true;
             this.submitButton.Click += new System.EventHandler(this.loginSubmitButton);
             // 
             // usernameTextBox
             // 
-            this.usernameTextBox.Location = new System.Drawing.Point(92, 127);
+            this.usernameTextBox.Location = new System.Drawing.Point(87, 128);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(205, 20);
-            this.usernameTextBox.TabIndex = 1;
+            this.usernameTextBox.Size = new System.Drawing.Size(341, 23);
+            this.usernameTextBox.TabIndex = 6;
             // 
             // passwordTextBox
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(92, 164);
+            this.passwordTextBox.Location = new System.Drawing.Point(87, 157);
             this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(205, 20);
-            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.Size = new System.Drawing.Size(341, 23);
+            this.passwordTextBox.TabIndex = 8;
             this.passwordTextBox.UseSystemPasswordChar = true;
             // 
             // usernameLabel
             // 
             this.usernameLabel.AutoSize = true;
-            this.usernameLabel.Location = new System.Drawing.Point(31, 130);
+            this.usernameLabel.Location = new System.Drawing.Point(8, 131);
             this.usernameLabel.Name = "usernameLabel";
-            this.usernameLabel.Size = new System.Drawing.Size(55, 13);
-            this.usernameLabel.TabIndex = 3;
-            this.usernameLabel.Text = "Username";
+            this.usernameLabel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.usernameLabel.Size = new System.Drawing.Size(73, 15);
+            this.usernameLabel.TabIndex = 5;
+            this.usernameLabel.Text = "&Username:";
             // 
             // passwordLabel
             // 
             this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(33, 171);
+            this.passwordLabel.Location = new System.Drawing.Point(11, 160);
             this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(53, 13);
-            this.passwordLabel.TabIndex = 3;
-            this.passwordLabel.Text = "Password";
+            this.passwordLabel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.passwordLabel.Size = new System.Drawing.Size(70, 15);
+            this.passwordLabel.TabIndex = 7;
+            this.passwordLabel.Text = "&Password:";
             // 
             // serverAddressTextBox
             // 
-            this.serverAddressTextBox.Location = new System.Drawing.Point(92, 51);
+            this.serverAddressTextBox.Location = new System.Drawing.Point(87, 99);
             this.serverAddressTextBox.Name = "serverAddressTextBox";
-            this.serverAddressTextBox.Size = new System.Drawing.Size(205, 20);
-            this.serverAddressTextBox.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 54);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Server Address";
+            this.serverAddressTextBox.Size = new System.Drawing.Size(278, 23);
+            this.serverAddressTextBox.TabIndex = 3;
             // 
             // serverPort
             // 
-            this.serverPort.Location = new System.Drawing.Point(92, 92);
+            this.serverPort.Location = new System.Drawing.Point(371, 99);
             this.serverPort.Maximum = new decimal(new int[] {
             65535,
             0,
             0,
             0});
             this.serverPort.Name = "serverPort";
-            this.serverPort.Size = new System.Drawing.Size(120, 20);
+            this.serverPort.Size = new System.Drawing.Size(57, 23);
             this.serverPort.TabIndex = 4;
             this.serverPort.Value = new decimal(new int[] {
             12086,
@@ -116,29 +111,55 @@
             0,
             0});
             // 
-            // label2
+            // labelServer
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 94);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Server Port";
+            this.labelServer.AutoSize = true;
+            this.labelServer.Location = new System.Drawing.Point(29, 102);
+            this.labelServer.Name = "labelServer";
+            this.labelServer.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.labelServer.Size = new System.Drawing.Size(52, 15);
+            this.labelServer.TabIndex = 2;
+            this.labelServer.Text = "&Server:";
+            // 
+            // labelMainInstruction
+            // 
+            this.labelMainInstruction.AutoSize = true;
+            this.labelMainInstruction.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMainInstruction.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(153)))));
+            this.labelMainInstruction.Location = new System.Drawing.Point(7, 9);
+            this.labelMainInstruction.Name = "labelMainInstruction";
+            this.labelMainInstruction.Padding = new System.Windows.Forms.Padding(10);
+            this.labelMainInstruction.Size = new System.Drawing.Size(293, 41);
+            this.labelMainInstruction.TabIndex = 0;
+            this.labelMainInstruction.Text = "Sign in to Password Manager to begin";
+            // 
+            // labelInstruction
+            // 
+            this.labelInstruction.Location = new System.Drawing.Point(8, 50);
+            this.labelInstruction.Name = "labelInstruction";
+            this.labelInstruction.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.labelInstruction.Size = new System.Drawing.Size(420, 38);
+            this.labelInstruction.TabIndex = 1;
+            this.labelInstruction.Text = "Connect to a server, enter your username and your password, and press Login to co" +
+    "ntinue.";
             // 
             // LoginDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 338);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(440, 249);
+            this.Controls.Add(this.labelInstruction);
+            this.Controls.Add(this.labelMainInstruction);
             this.Controls.Add(this.serverPort);
             this.Controls.Add(this.passwordLabel);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelServer);
             this.Controls.Add(this.usernameLabel);
             this.Controls.Add(this.passwordTextBox);
             this.Controls.Add(this.serverAddressTextBox);
             this.Controls.Add(this.usernameTextBox);
             this.Controls.Add(this.submitButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "LoginDialog";
             this.Text = "Login";
             ((System.ComponentModel.ISupportInitialize)(this.serverPort)).EndInit();
@@ -155,9 +176,10 @@
         private System.Windows.Forms.Label usernameLabel;
         private System.Windows.Forms.Label passwordLabel;
         private System.Windows.Forms.TextBox serverAddressTextBox;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.NumericUpDown serverPort;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelServer;
+        private System.Windows.Forms.Label labelMainInstruction;
+        private System.Windows.Forms.Label labelInstruction;
     }
 }
 
