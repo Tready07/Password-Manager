c82a4bdd64ca233262de821e5a086e463b8f7776
Tready07 <tready246@gmail.com>
2018-03-18 00:09:27 -0500

    DeleteUsername Request and response
    The DeleteUsername Request and response have been added!

---
+++ Networking/Requests/DeleteUsernameRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking.Requests
+{
+    [Serializable]
+    class DeleteUsernameRequest : MessageBase
+    {
+        public const int MessageID = 4;
+
+        public DeleteUsernameRequest(Shared.Application app) :
+            base(MessageID, MessageType.Request)
+        {
+            application = app;
+        }
+        public Shared.Application application;
+
+        public override byte[] ToByteArray()
+        {
+            using (var stream = new MemoryStream())
+            {
+                var serializer = new BinaryFormatter();
+                serializer.Serialize(stream, this);
+
+                return stream.ToArray();
+            }
+        }
+    }
+}
---
+++ Networking/Responses/DeleteUsernameResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking.Responses
+{
+    [Serializable]
+    class DeleteUsernameResponse : MessageBase
+    {
+        public const int MessageID = 3;
+
+        public DeleteUsernameResponse(Shared.Application app) :
+            base(MessageID, MessageType.Response)
+        {
+            application = app;
+        }
+
+        public Shared.Application application { get; set; }
+
+        public override byte[] ToByteArray()
+        {
+            using (var stream = new MemoryStream())
+            {
+                var serializer = new BinaryFormatter();
+                serializer.Serialize(stream, this);
+
+                return stream.ToArray();
+            }
+        }
+    }
+
+
+}
