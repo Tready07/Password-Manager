009a0b706ffd51f8d18706c592434886f4a584b1
Richard Lim <rclim95@outlook.com>
2018-03-18 22:19:48 -0500

    Change the UI for NewApplicationForm
    - Make it similar to the appearance of the TaskDialog in Windows Vista
      and higher (which makes it consistently similar in appearance to the
      LoginDialog)
    - Make buttonSubmit the AcceptButton and cancelButton be the
      CancelButton (so pressing [ENTER] will activate buttonSubmit.Click and
      pressing [ESC] will activate cancelButton.Click and close the dialog)
    - Right-align the label against the form control

--- Password Manager/NewApplicationForm.Designer.cs
+++ Password Manager/NewApplicationForm.Designer.cs
@@ -36,58 +36,62 @@
             this.passwordLabel = new System.Windows.Forms.Label();
             this.appTypeComboBox = new System.Windows.Forms.ComboBox();
             this.appTypeLabel = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonSubmit = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
+            this.labelMainInstruction = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // usernameTextBox
             // 
-            this.usernameTextBox.Location = new System.Drawing.Point(107, 102);
+            this.usernameTextBox.Location = new System.Drawing.Point(130, 99);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(186, 20);
-            this.usernameTextBox.TabIndex = 3;
+            this.usernameTextBox.Size = new System.Drawing.Size(328, 23);
+            this.usernameTextBox.TabIndex = 4;
             // 
             // appNameTextBox
             // 
-            this.appNameTextBox.Location = new System.Drawing.Point(107, 60);
+            this.appNameTextBox.Location = new System.Drawing.Point(130, 70);
             this.appNameTextBox.Name = "appNameTextBox";
-            this.appNameTextBox.Size = new System.Drawing.Size(186, 20);
-            this.appNameTextBox.TabIndex = 1;
+            this.appNameTextBox.Size = new System.Drawing.Size(328, 23);
+            this.appNameTextBox.TabIndex = 2;
             // 
             // applicationNameLabel
             // 
             this.applicationNameLabel.AutoSize = true;
-            this.applicationNameLabel.Location = new System.Drawing.Point(11, 63);
+            this.applicationNameLabel.Location = new System.Drawing.Point(8, 73);
             this.applicationNameLabel.Name = "applicationNameLabel";
-            this.applicationNameLabel.Size = new System.Drawing.Size(90, 13);
-            this.applicationNameLabel.TabIndex = 0;
-            this.applicationNameLabel.Text = "Application Name";
+            this.applicationNameLabel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.applicationNameLabel.Size = new System.Drawing.Size(116, 15);
+            this.applicationNameLabel.TabIndex = 1;
+            this.applicationNameLabel.Text = "Application &Name:";
             // 
             // usernameLabel
             // 
             this.usernameLabel.AutoSize = true;
-            this.usernameLabel.Location = new System.Drawing.Point(15, 105);
+            this.usernameLabel.Location = new System.Drawing.Point(51, 102);
             this.usernameLabel.Name = "usernameLabel";
-            this.usernameLabel.Size = new System.Drawing.Size(55, 13);
-            this.usernameLabel.TabIndex = 2;
-            this.usernameLabel.Text = "Username";
+            this.usernameLabel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.usernameLabel.Size = new System.Drawing.Size(73, 15);
+            this.usernameLabel.TabIndex = 3;
+            this.usernameLabel.Text = "&Username:";
             // 
             // pwTextBox
             // 
-            this.pwTextBox.Location = new System.Drawing.Point(107, 142);
+            this.pwTextBox.Location = new System.Drawing.Point(130, 128);
             this.pwTextBox.Name = "pwTextBox";
-            this.pwTextBox.Size = new System.Drawing.Size(186, 20);
-            this.pwTextBox.TabIndex = 5;
+            this.pwTextBox.Size = new System.Drawing.Size(328, 23);
+            this.pwTextBox.TabIndex = 6;
             this.pwTextBox.UseSystemPasswordChar = true;
             // 
             // passwordLabel
             // 
             this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(15, 145);
+            this.passwordLabel.Location = new System.Drawing.Point(54, 131);
             this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(53, 13);
-            this.passwordLabel.TabIndex = 4;
-            this.passwordLabel.Text = "Password";
+            this.passwordLabel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.passwordLabel.Size = new System.Drawing.Size(70, 15);
+            this.passwordLabel.TabIndex = 5;
+            this.passwordLabel.Text = "&Password:";
             // 
             // appTypeComboBox
             // 
@@ -100,47 +104,65 @@
             "Shopping",
             "Other",
             "Add Custom Type"});
-            this.appTypeComboBox.Location = new System.Drawing.Point(107, 183);
+            this.appTypeComboBox.Location = new System.Drawing.Point(130, 157);
             this.appTypeComboBox.Name = "appTypeComboBox";
-            this.appTypeComboBox.Size = new System.Drawing.Size(117, 21);
-            this.appTypeComboBox.TabIndex = 7;
+            this.appTypeComboBox.Size = new System.Drawing.Size(328, 23);
+            this.appTypeComboBox.TabIndex = 8;
             this.appTypeComboBox.SelectedIndexChanged += new System.EventHandler(this.appTypeComboBox_SelectedIndexChanged);
             // 
             // appTypeLabel
             // 
             this.appTypeLabel.AutoSize = true;
-            this.appTypeLabel.Location = new System.Drawing.Point(15, 186);
+            this.appTypeLabel.Location = new System.Drawing.Point(15, 160);
             this.appTypeLabel.Name = "appTypeLabel";
-            this.appTypeLabel.Size = new System.Drawing.Size(86, 13);
-            this.appTypeLabel.TabIndex = 6;
-            this.appTypeLabel.Text = "Application Type";
+            this.appTypeLabel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.appTypeLabel.Size = new System.Drawing.Size(109, 15);
+            this.appTypeLabel.TabIndex = 7;
+            this.appTypeLabel.Text = "Application &Type:";
             // 
-            // button1
+            // buttonSubmit
             // 
-            this.button1.Location = new System.Drawing.Point(218, 222);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Submit";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.submitButton);
+            this.buttonSubmit.Location = new System.Drawing.Point(278, 196);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(87, 27);
+            this.buttonSubmit.TabIndex = 9;
+            this.buttonSubmit.Text = "&Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.submitButton);
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(107, 222);
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(371, 196);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(75, 23);
-            this.cancelButton.TabIndex = 8;
-            this.cancelButton.Text = "Cancel";
+            this.cancelButton.Size = new System.Drawing.Size(87, 27);
+            this.cancelButton.TabIndex = 10;
+            this.cancelButton.Text = "&Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // labelMainInstruction
+            // 
+            this.labelMainInstruction.AutoSize = true;
+            this.labelMainInstruction.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMainInstruction.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(153)))));
+            this.labelMainInstruction.Location = new System.Drawing.Point(7, 9);
+            this.labelMainInstruction.Name = "labelMainInstruction";
+            this.labelMainInstruction.Padding = new System.Windows.Forms.Padding(10);
+            this.labelMainInstruction.Size = new System.Drawing.Size(301, 41);
+            this.labelMainInstruction.TabIndex = 0;
+            this.labelMainInstruction.Text = "Add a new username for an application";
             // 
             // NewApplicationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AcceptButton = this.buttonSubmit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(350, 300);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(470, 235);
+            this.Controls.Add(this.labelMainInstruction);
             this.Controls.Add(this.cancelButton);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSubmit);
             this.Controls.Add(this.appTypeComboBox);
             this.Controls.Add(this.appTypeLabel);
             this.Controls.Add(this.passwordLabel);
@@ -149,6 +171,7 @@
             this.Controls.Add(this.appNameTextBox);
             this.Controls.Add(this.pwTextBox);
             this.Controls.Add(this.usernameTextBox);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "NewApplicationForm";
             this.Text = "New Application";
             this.ResumeLayout(false);
@@ -166,7 +189,8 @@
         private System.Windows.Forms.Label passwordLabel;
         private System.Windows.Forms.ComboBox appTypeComboBox;
         private System.Windows.Forms.Label appTypeLabel;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonSubmit;
         private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label labelMainInstruction;
     }
 }
