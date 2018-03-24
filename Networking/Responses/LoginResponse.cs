0cbb5642f67ddd98d7610d6ef343c9d574114afd
Tready07 <tready246@gmail.com>
2018-03-23 22:05:51 -0500

    Login Response
    see above commit

---
+++ Networking/Responses/LoginResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking.Responses
+{
+    [Serializable]
+    public class LoginResponse : MessageBase
+    {
+        public const int MessageID = 7;
+
+        public LoginResponse(bool admin) :
+            base(MessageID, MessageType.Response)
+        {
+            isAdmin = admin;
+        }
+        public bool isAdmin { get; set; } = false;
+
+        public override byte[] ToByteArray()
+        {
+            using (var stream = new MemoryStream())
+            {
+                var serializer = new BinaryFormatter();
+                serializer.Serialize(stream, this);
+
+                return stream.ToArray();
+            }
+        }
+    }
+}
