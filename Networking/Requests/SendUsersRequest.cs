35a78f54b7048ba137821c30c547d1c5aaa06685
Tready07 <tready246@gmail.com>
2018-03-24 21:46:52 -0500

    Send users Request and Response
    sending users request and response have been implemented

---
+++ Networking/Requests/SendUsersRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking.Requests
+{
+    [Serializable]
+    public class SendUsersRequest : MessageBase
+    {
+        public const int MessageID = 8;
+        public SendUsersRequest():
+            base(MessageID, MessageType.Request)
+        {
+
+        }
+        public override byte[] ToByteArray()
+        {
+            using (var stream = new MemoryStream())
+            {
+                var serializer = new BinaryFormatter();
+                serializer.Serialize(stream, this);
+
+                return stream.ToArray();
+            }
+        }
+    }
+}
---
+++ Networking/Responses/SendUsersResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking.Responses
+{
+    [Serializable]
+    public class SendUsersResponse  : MessageBase
+    {
+        public const int MessageID = 8;
+        public SendUsersResponse(Shared.Username [] usernames) :
+            base(MessageID, MessageType.Response)
+        {
+            users = usernames;
+        }
+
+        public Shared.Username[] users { get; set; }
+
+        public override byte[] ToByteArray()
+        {
+            using (var stream = new MemoryStream())
+            {
+                var serializer = new BinaryFormatter();
+                serializer.Serialize(stream, this);
+
+                return stream.ToArray();
+            }
+        }
+    }
+}
