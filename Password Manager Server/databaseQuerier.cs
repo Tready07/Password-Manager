e5d540df3542179d97e57b897c8d8b6a3d5ab54c
Tready07 <Tready246@gmail.com>
2018-04-01 00:20:47 -0500

    catch errors
    the databaseQuerier has been modified to catch errors and return booleans to indicate success or failure for many queries
    this is so that the message handler will know to throw error messages to the client.

--- Password Manager Server/databaseQuerier.cs
+++ Password Manager Server/databaseQuerier.cs
@@ -143,15 +143,21 @@ namespace Password_Manager_Server
          *      sets the password for a specific username under the specific user
          *      NOTE: the password should be encrypted
          */      
-        public void setPassword(String appName, String username, String user, String password)
+        public bool setPassword(String appName, String username, String user, String password)
         {
-            String sqlString = "UPDATE applications SET password = @password WHERE application = @appName AND username = @username AND name = @user";
-            SQLiteCommand command = new SQLiteCommand(sqlString, dbConnection);
-            command.Parameters.AddWithValue("@name", user);
-            command.Parameters.AddWithValue("@appName", appName);
-            command.Parameters.AddWithValue("@username", username);
-            command.Parameters.AddWithValue("@password", password);
-            command.ExecuteNonQuery();
+            try{
+                String sqlString = "UPDATE applications SET password = @password WHERE application = @appName AND username = @username AND name = @user";
+                SQLiteCommand command = new SQLiteCommand(sqlString, dbConnection);
+                command.Parameters.AddWithValue("@name", user);
+                command.Parameters.AddWithValue("@appName", appName);
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", password);
+                command.ExecuteNonQuery();
+                return true;
+                }
+            catch{
+                return false;
+            }
         }
 
         /**
@@ -159,15 +165,22 @@ namespace Password_Manager_Server
          *      sets the password for a specific username under the specific user
          *      NOTE: the password should be encrypted
          */
-        public void setPassword(Shared.Application app, String user)
+        public bool setPassword(Shared.Application app, String user)
         {
-            String sqlString = "UPDATE applications SET password = @password WHERE application = @appName AND username = @username AND name = @user";
-            SQLiteCommand command = new SQLiteCommand(sqlString, dbConnection);
-            command.Parameters.AddWithValue("@user", user);
-            command.Parameters.AddWithValue("@appName", app.Name);
-            command.Parameters.AddWithValue("@username", app.Usernames[0].name);
-            command.Parameters.AddWithValue("@password", app.Usernames[0].password);
-            command.ExecuteNonQuery();
+            try{
+                String sqlString = "UPDATE applications SET password = @password WHERE application = @appName AND username = @username AND name = @user";
+                SQLiteCommand command = new SQLiteCommand(sqlString, dbConnection);
+                command.Parameters.AddWithValue("@user", user);
+                command.Parameters.AddWithValue("@appName", app.Name);
+                command.Parameters.AddWithValue("@username", app.Usernames[0].name);
+                command.Parameters.AddWithValue("@password", app.Usernames[0].password);
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         /**
@@ -202,14 +215,20 @@ namespace Password_Manager_Server
          *      removes a username and his info from the database.
          *      
          */
-        public void removeUsername(Shared.Application app, String user)
+        public bool removeUsername(Shared.Application app, String user)
         {
-            String sqlString = "DELETE FROM applications WHERE name = @name AND application = @appname AND username = @username";
-            SQLiteCommand command = new SQLiteCommand(sqlString, dbConnection);
-            command.Parameters.AddWithValue("@name", user);
-            command.Parameters.AddWithValue("@appName", app.Name);
-            command.Parameters.AddWithValue("@username", app.Usernames[0].name);
-            command.ExecuteNonQuery();
+            try{
+                String sqlString = "DELETE FROM applications WHERE name = @name AND application = @appname AND username = @username";
+                SQLiteCommand command = new SQLiteCommand(sqlString, dbConnection);
+                command.Parameters.AddWithValue("@name", user);
+                command.Parameters.AddWithValue("@appName", app.Name);
+                command.Parameters.AddWithValue("@username", app.Usernames[0].name);
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch{
+                return false;
+            }
         }
 
         public String getSalt(String user)
