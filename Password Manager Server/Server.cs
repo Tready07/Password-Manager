92b6f83d418c72c25d4e9ebd48b44edd3f4f7516
Richard Lim <rclim95@outlook.com>
2018-04-07 15:53:25 -0500

    Refactor code for accepting clients & handling messages on the server
    - Improve code readability by moving the anonymous method for accepting
      clients into its own method (Server.HandleNewClient)
    - Improve code readability by moving the anonymous method for handling
      messages into its own method (Server.HandleNewMessages)
    - Perform proper cleanup when Server.Stop is called by calling
      TcpListener.Stop so that the server will stop accepting clients
      - Now that we're doing that, surround the code block that calls
        TcpListener.AcceptTcpClientAsync() in Server.HandleNewClient() with
        a try/catch of the InvalidOperationException exception (since this
        exception is thrown by TcpListener.AcceptTcpClientAsync when
        TcpListener.Stop is called)
    - Rewrite the code for handling new messages to use the new
      MessageReader to process the messages' header and its payload instead
      of doing it ourselves
      - MessageReader will automatically take care of cases where a
        message's payload might be too big to fit in a single TCP packet
        (i.e., it might take multiple Socket.Select calls until the entire
        message payload is read), fixing a possible case where a message has
        been processed earlier (even though the entire message hasn't been
        transferred) since we assumed the message can fit within a
        4096-bytes buffer
    - Change some Trace.WriteLine statements to include the IP address of
      the client that has connected, disconnected, and sent a message to the
      server

--- Password Manager Server/Server.cs
+++ Password Manager Server/Server.cs
@@ -51,141 +51,11 @@ namespace Password_Manager_Server
         {
             this.tcpListener.Start();
 
-            var acceptClientTask = Task.Factory.StartNew(async () =>
-            {
-                while (!this.cancellationToken.IsCancellationRequested)
-                {
-                    var tcpClient = await this.tcpListener.AcceptTcpClientAsync();
+            var handleClient = this.HandleNewClient(this.cancellationToken.Token);
+            var handleMessages = this.HandleNewMessages(this.cancellationToken.Token);
+            var tasks = new Task[] { handleClient, handleMessages };
 
-                    // Print some information about the client that joined
-                    var ipAddress = (IPEndPoint)tcpClient.Client.RemoteEndPoint;
-                    Debug.WriteLine($"New client joined: {ipAddress.Address.ToString()}", "Server");
-
-                    lock (this.clientListLock)
-                    {
-                        this.clientList.Add(new ClientSession(tcpClient));
-                    }
-                }
-
-                Debug.WriteLine("Server is no longer accepting clients.");
-            }, this.cancellationToken.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
-
-            var handleClientMessages = Task.Factory.StartNew(() =>
-            {
-                const int Timeout = 100;
-
-                List<ReadyClient> pendingReadingTasks = new List<ReadyClient>();
-                byte[] buffer = new byte[4096];
-                MessageHandler handler = new MessageHandler();
-                while (!this.cancellationToken.IsCancellationRequested)
-                {
-                    if (this.clientList.Count == 0)
-                    {
-                        // Put the thread to sleep for a few milliseconds if we don't have any clients
-                        // to process.
-                        Thread.Sleep(Timeout);
-                        continue;
-                    }
-
-                    // Go through each client and check to see if there are any data to be read in
-                    // by calling Socket.Select.
-                    System.Collections.IList clientToRead = null;
-                    lock (this.clientListLock)
-                    {
-                        clientToRead = this.clientList.Select(cs => cs.Client.Client).ToList();
-                    }
-                    
-                    // Socket.Select will modify the clientToRead list so that it contains only
-                    // the clients that are ready to read. Note that we set the timeout to 100ms so
-                    // that it'll wait 100ms on each socket to see if it's available for reading
-                    // before moving on to the next one (don't do it indefinitely, in case a new
-                    // client joins).
-                    Socket.Select(clientToRead, null, null, Timeout);
-                    foreach (Socket client in clientToRead)
-                    {
-                        var networkStream = new NetworkStream(client);
-
-                        // Is there any data available to be read? If not, then that means the
-                        // socket has been disconnected.
-                        if (client.Available > 0)
-                        {
-                            pendingReadingTasks.Add(new ReadyClient(networkStream.ReadAsync(buffer, 0, buffer.Length),client));
-                        }
-                        else
-                        {
-                            var ipAddress = (IPEndPoint)client.RemoteEndPoint;
-                            Debug.WriteLine($"Client disconnected: {ipAddress.Address}", "Server");
-
-                            lock (this.clientListLock)
-                            {
-                                this.clientList.Remove(this.clientList.Single(cs => cs.Client.Client == client));
-                            }
-                        }
-                    }
-
-                    
-                    // Start processing the pending reading tasks   
-                    int i = pendingReadingTasks.Count - 1;
-                    while (pendingReadingTasks.Count > 0)
-                    {
-                        var currentReadyClient = pendingReadingTasks[i].task.GetAwaiter().GetResult();
-
-                        // "Process" the message
-                        int actualBytesRead = pendingReadingTasks[i].task.Result;
-                        Debug.WriteLine($"We received bytes: {actualBytesRead.ToString()}", "Server");
-
-                        try
-                        {
-                            using (var stream = new MemoryStream(buffer))
-                            {
-                                var messageHeader = MessageUtils.DeserializeMessageHeader(stream);
-
-                                // Make sure that we have enough data to actually process this message
-                                if (actualBytesRead - MessageHeader.HeaderSize < messageHeader.Size)
-                                {
-                                    // TODO: Actually handle this properly, i.e., skip it, keep
-                                    //       track of the message header we just read in, and then
-                                    //       read remaining data streamed until we have enough data
-                                    //       to properly process this message.
-                                    Debug.WriteLine("There is not enough data to read in the message payload.", "Server");
-                                    Debug.WriteLine("Skipping message...");
-                                }
-                                else
-                                {
-                                    var socket = pendingReadingTasks[i].socket;
-                                    bool succeeded = handler.handleMessage(buffer, 
-                                        this.clientList.Single(cs => cs.Client.Client == pendingReadingTasks[i].socket),
-                                        messageHeader);
-
-                                    // TODO: If succeeded is false, we should some kind of ErrorResponse.
-                                    if (!succeeded && messageHeader.ID == LoginRequest.MessageID)
-                                    {
-                                        // The login failed, so force a disconnect.
-                                        socket.Shutdown(SocketShutdown.Both);
-                                        socket.Disconnect(true);
-                                        lock (this.clientListLock)
-                                        {
-                                            this.clientList.Remove(this.clientList.Single(cs => cs.Client.Client == socket));
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        catch (BadHeaderException e)
-                        {
-                            Debug.WriteLine("Error while attempting to decode header:\n" + e, "Server");
-                        }
-
-                        pendingReadingTasks.Remove(pendingReadingTasks[i]);
-                        i--;
-                    }
-                }
-
-                Debug.WriteLine("Server is no longer handling clients.");
-            }, this.cancellationToken.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
-
-            var tasksToWait = new Task[] { acceptClientTask, handleClientMessages };
-            Task.WaitAll(tasksToWait);
+            Task.WaitAll(tasks);
 
             Debug.WriteLine("The server has been stopped.");
         }
@@ -195,7 +65,121 @@ namespace Password_Manager_Server
         /// </summary>
         public void Stop()
         {
+            this.tcpListener.Stop();
             this.cancellationToken.Cancel();
+        }
+
+        private ClientSession FindClientBySocket(Socket socket)
+        {
+            return this.clientList.SingleOrDefault(cs => cs.Client.Client == socket);
+        }
+
+        private async Task HandleNewClient(CancellationToken token)
+        {
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    var tcpClient = await this.tcpListener.AcceptTcpClientAsync();
+
+                    // Print some information about the client that joined
+                    var ipAddress = (IPEndPoint)tcpClient.Client.RemoteEndPoint;
+                    Debug.WriteLine($"{ipAddress.Address}: Client Joined", "Server");
+
+                    lock (this.clientListLock)
+                    {
+                        this.clientList.Add(new ClientSession(tcpClient));
+                    }
+                }
+            }
+            catch (ObjectDisposedException ex) when (ex.ObjectName == "System.Net.Sockets.Socket")
+            {
+                // This exception gets thrown by TcpListener.AcceptTcpClientAsync when
+                // TcpListener.Stop is called.
+                //
+                // TODO: Is there a better way to properly cancel TcpListener.AcceptTcpClientAsync?
+                Debug.WriteLine("Server is no longer accepting clients.");
+            }
+        }
+
+        private async Task HandleNewMessages(CancellationToken token)
+        {
+            const int Timeout = 100;
+
+            List<ReadyClient> pendingReadingTasks = new List<ReadyClient>();
+            MessageHandler handler = new MessageHandler();
+            while (!token.IsCancellationRequested)
+            {
+                if (this.clientList.Count == 0)
+                {
+                    // Put the thread to sleep for a few milliseconds if we don't have any clients
+                    // to process.
+                    await Task.Delay(Timeout);
+                    continue;
+                }
+
+                // Go through each client and check to see if there are any data to be read in
+                // by calling Socket.Select.
+                System.Collections.IList clientToRead = null;
+                lock (this.clientListLock)
+                {
+                    clientToRead = this.clientList.Select(cs => cs.Client.Client).ToList();
+                }
+
+                // Socket.Select will modify the clientToRead list so that it contains only
+                // the clients that are ready to read. Note that we set the timeout to 100ms so
+                // that it'll wait 100ms on each socket to see if it's available for reading
+                // before moving on to the next one (don't do it indefinitely, in case a new
+                // client joins).
+                Socket.Select(clientToRead, null, null, Timeout);
+                foreach (Socket client in clientToRead)
+                {
+                    var networkStream = new NetworkStream(client);
+                    var clientSession = this.FindClientBySocket(client);
+                    var ipAddress = (IPEndPoint)client.RemoteEndPoint;
+                    var reader = clientSession.Reader;
+
+                    if (client.Available == 0)
+                    { 
+                        // The client has disconnected. Move on to the next client.
+                        Trace.WriteLine($"{ipAddress.Address}: Client Disconnected", "Server");
+                        this.RemoveClient(clientSession);
+                        continue;
+                    }
+
+                    // Process the message
+                    await reader.Process(networkStream);
+                    if (!reader.IsMessageReady)
+                    {
+                        continue;
+                    }
+
+                    var header = reader.Header.Value;
+
+                    Trace.WriteLine($"{ipAddress.Address}: Message ID {header.ID} Received ({header.Size} byte(s))", "Server");
+
+                    bool succeeded = handler.handleMessage(clientSession);
+                    if (!succeeded && header.ID == LoginRequest.MessageID)
+                    {
+                        // The login failed, so force a disconnect.
+                        this.RemoveClient(clientSession);
+                        continue;
+                    }
+
+                    reader.ProcessNextMessage();
+                }
+            }
+
+            Debug.WriteLine("Server is no longer reading messages");
+        }
+
+        private void RemoveClient(ClientSession client)
+        {
+            client.Client.Close();
+            lock (this.clientListLock)
+            {
+                this.clientList.Remove(client);
+            }
         }
     }
 }
